73aef409567d0e5f2374a451f415bfbbaa7af610
Clygro <32335943+Clygro@users.noreply.github.com>
2022-08-08 18:36:45 +0100

    Update design.ts

--- src/constants/design.ts
+++ src/constants/design.ts
@@ -1,17 +1,17 @@
-export const DEFAULT_TAB_MARGIN_TOP = 4;
-export const COMPACT_TAB_MARGIN_TOP = 3;
+export const DEFAULT_TAB_MARGIN_TOP = 1;
+export const COMPACT_TAB_MARGIN_TOP = 1;
 
-export const DEFAULT_TAB_HEIGHT = 32;
-export const COMPACT_TAB_HEIGHT = 32;
+export const DEFAULT_TAB_HEIGHT = 16;
+export const COMPACT_TAB_HEIGHT = 16;
 
 // Toolbar
-export const TOOLBAR_HEIGHT = 42;
+export const TOOLBAR_HEIGHT = 32;
 
-export const TOOLBAR_BUTTON_WIDTH = 36;
-export const TOOLBAR_BUTTON_HEIGHT = 32;
+export const TOOLBAR_BUTTON_WIDTH = 24;
+export const TOOLBAR_BUTTON_HEIGHT = 24;
 
-export const ADD_TAB_BUTTON_WIDTH = 28;
-export const ADD_TAB_BUTTON_HEIGHT = 28;
+export const ADD_TAB_BUTTON_WIDTH = 24;
+export const ADD_TAB_BUTTON_HEIGHT = 24;
 
 export const DEFAULT_TITLEBAR_HEIGHT =
   DEFAULT_TAB_MARGIN_TOP + DEFAULT_TAB_HEIGHT;
@@ -21,11 +21,11 @@ export const COMPACT_TITLEBAR_HEIGHT =
 export const VIEW_Y_OFFSET = TOOLBAR_HEIGHT + DEFAULT_TITLEBAR_HEIGHT;
 
 // Widths
-export const WINDOWS_BUTTON_WIDTH = 45;
+export const WINDOWS_BUTTON_WIDTH = 24;
 export const MENU_WIDTH = 330;
 
 // Dialogs
-export const DIALOG_MIN_HEIGHT = 130;
-export const DIALOG_MARGIN = 16;
+export const DIALOG_MIN_HEIGHT = 100;
+export const DIALOG_MARGIN = 4;
 export const DIALOG_TOP = 0;
 export const DIALOG_MARGIN_TOP = 0;
